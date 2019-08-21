d4fff2f31b822cc69d9b58e28214232295aedb68
Kirill Goncharov <kdgoncharov@gmail.com>
2019-08-21 15:15:17 +0300

    Bump version: 1.0.0 → 2.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='beancount-exchangerates',
-    version='1.0.0',
+    version='2.0.0',
     description='Beancount Exchange Rates',
     packages=['beancount_exchangerates'],
     license='GPLv3',
