93dfc7ba750f3aea665c3e79d6ad4bbf7309a567
xuhcc <xuhcc@cryptolab.net>
2021-08-12 16:46:30 +0300

    Bump version: 3.0.0 → 3.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='beancount-exchangerates',
-    version='3.0.0',
+    version='3.1.0',
     description='Beancount Exchange Rates',
     packages=['beancount_exchangerates'],
     license='GPLv3',
